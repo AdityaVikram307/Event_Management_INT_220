cf180d01c345972acb55559404bef4519866ef9d
AdityaVikram307 <63582134+AdityaVikram307@users.noreply.github.com>
2022-04-10 13:59:37 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- SITE TITTLE -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ERROR</title>
+  <?php
+  include('header.php');
+  ?>
+</head>
+
+<body class="body-wrapper">
+  <section class="page-title bg-title overlay-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <div class="title">
+            <h3>Error Page</h3>
+          </div>
+          <ol class="breadcrumb p-0 m-0">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Error Page</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--====  End of Page Title  ====-->
+
+
+  <!--===================================
+=            Error Section            =
+====================================-->
+
+  <section class="section error">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 m-auto">
+          <div class="block text-center">
+            <img src="images/404.png" class="img-fluid" alt="404">
+            <h3>Oops!... <span>Page Not Found.</span></h3>
+            <a href="index.html" class="btn btn-main-md">Go to home</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--====  End of Error Section  ====-->
+
+
+
+
+
+  <!--====  End of Google Map  ====-->
+  <?php
+  include('footer.php');
+  ?>
+</body>
+
+</html>
