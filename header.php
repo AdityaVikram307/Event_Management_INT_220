4d0241d3a142692e62e7c6269df21ab87c0b282e
AdityaVikram307 <63582134+AdityaVikram307@users.noreply.github.com>
2022-04-10 12:45:07 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="plugins/font-awsome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="plugins/slick/slick.css" rel="stylesheet">
+    <link href="plugins/slick/slick-theme.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="scss/_typography.scss">
+    <link rel="stylesheet" href="scss/components/_navigation.scss">
+    <link href="images/favicon2.png" rel="shortcut icon">
+</head>
+
+<body>
+    <nav class="navbar main-nav border-less fixed-top navbar-expand-lg p-0">
+        <div class="container-fluid p-0">
+            <a class="navbar-brand" href="index.php">
+                <h5 id="logo">Sur<span style="color: orangered;">ge</span></h5>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item dropdown active dropdown-slide">
+                        <a class="nav-link" style="color: black;" href="index.php">Home
+                            <span>/</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="speakers.php">Speakers
+                            <span>/</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown dropdown-slide">
+                        <a class="nav-link" href="#" data-toggle="dropdown">Pages<span>/</span></a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="about-us.php">About Us</a>
+                            <a class="dropdown-item" href="chief-speaker.php">Chief Speaker</a>
+                            <a class="dropdown-item" href="gallery.php">Gallery</a>
+                            <a class="dropdown-item" href="testimonial.php">Testimonial</a>
+                            <a class="dropdown-item" href="pricing.php">Tickets</a>
+                            <a class="dropdown-item" href="FAQ.php">FAQ</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="schedule.php">Schedule<span>/</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sponsors.php">Sponsors<span>/</span></a>
+                    </li>
+                    <li class="nav-item dropdown dropdown-slide">
+                        <a class="nav-link" href="news1.php">News
+                            <span>/</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+                <a style="background: #ff6600;" href="pricing.php" class="ticket">
+                    <img src="images/icon/ticket.png" alt="ticket">
+                    <span>Buy Ticket</span>
+                </a>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
